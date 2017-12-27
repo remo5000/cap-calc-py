444b7c7146643af9f4f16bd58a05b7fe8cf02062
remo5000 <vigneshshankar@gmail.com>
2017-12-27 11:18:51 +0800

    Initial commit

---
+++ capcalc.py
@@ -0,0 +1,14 @@
+from math import ceil
+input_ended = False
+number_of_grades = 0
+total = 0
+while not input_ended:
+    inp = input("Enter a grade point, or 'stop' to end: ")
+    if inp == 'stop':
+        input_ended = True
+    else:
+        total += float(inp) 
+        number_of_grades += 1
+cap = total / number_of_grades
+cap = ceil(float(cap)*10)/10   
+print("your cap is", cap)
