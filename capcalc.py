fd74b9df7f42e440780e7ace9ce7a4481e8195f8
remo5000 <vigneshshankar@gmail.com>
2017-12-31 23:26:59 +0800

    replace while True with while inp != 'stop'

--- capcalc.py
+++ capcalc.py
@@ -2,13 +2,11 @@ from math import ceil
 
 def input_all_mods():
     all_mods = []
-    while True:
-        inp = input("input mod '<name> <MC count> <grade point>' or 'stop' to end: ")
-        if inp == 'stop':
-            break
-        else:
-            name, mc, grade = inp.split(' ')[0], int(inp.split(' ')[1]), float(inp.split(' ')[2])
-            all_mods.append({'name': name, 'mc': mc, 'gradepoint': grade})
+    inp = input("input mod '<name> <MC count> <grade point>' or 'stop' to end: ")
+    while inp.lower() != 'stop':
+        name, mc, grade = inp.split(' ')[0], int(inp.split(' ')[1]), float(inp.split(' ')[2])
+        all_mods.append({'name': name, 'mc': mc, 'gradepoint': grade})
+        inp = input("input mod '<name> <MC count> <grade point>' or 'stop' to end: ") 
     return all_mods
 
 def calc_cap(all_mods):
@@ -28,13 +26,11 @@ def su(all_mods):
         mc = str(mod['mc'])
         gp = str(mod['gradepoint'])
         print("Index: " + i + "\t" + name + "\t" + mc + " MCs" + "\t" + "Grade: " + gp)
-    while True:
+    inp = input("input indices to s/u or 'stop' to end: ")
+    while inp != 'stop':
+        index = int(inp)
+        del all_mods[index]    
         inp = input("input indices to s/u or 'stop' to end: ")
-        if inp == 'stop':
-            break
-        else:
-            index = int(inp)
-            del all_mods[index]
     return all_mods
 
 all_mods = input_all_mods()
