b9f1c86703049f31ef6077851bc5f72985a028e8
remo5000 <vigneshshankar@gmail.com>
2018-01-01 00:43:27 +0800

    Merged with a pull request from ningyuansg to fix syntax and a few bugs

--- capcalc.py
+++ capcalc.py
@@ -1,12 +1,24 @@
 from math import ceil
 
+INPUT_PROMPT = "input mod '<name> <MC count> <grade point>' or 'stop' to end: "
+INDICES_PROMPT = "input indices to s/u or 'stop' to end: " 
+SU_PROMPT = "do you want to s/u? y/n: "
+
 def input_all_mods():
     all_mods = []
+<<<<<<< HEAD
     inp = input("input mod '<name> <MC count> <grade point>' or 'stop' to end: ")
     while inp.lower() != 'stop':
         name, mc, grade = inp.split(' ')[0], int(inp.split(' ')[1]), float(inp.split(' ')[2])
         all_mods.append({'name': name, 'mc': mc, 'gradepoint': grade})
         inp = input("input mod '<name> <MC count> <grade point>' or 'stop' to end: ") 
+=======
+    inp = input(INPUT_PROMPT)
+    while inp.lower() != 'stop':
+        name, mc, grade = inp.split(' ')
+        all_mods.append({'name': name, 'mc': int(mc), 'gradepoint': float(grade)})
+        inp = input(INPUT_PROMPT)
+>>>>>>> 42b2096786153535428ed8581e6d4ea8ab80e8b1
     return all_mods
 
 def calc_cap(all_mods):
@@ -21,6 +33,7 @@ def calc_cap(all_mods):
 
 def su(all_mods):
     for index, mod in enumerate(all_mods):
+<<<<<<< HEAD
         i = str(index)
         name = mod['name']
         mc = str(mod['mc'])
@@ -32,9 +45,19 @@ def su(all_mods):
         del all_mods[index]    
         inp = input("input indices to s/u or 'stop' to end: ")
     return all_mods
+=======
+        print("Index: {}\t{}\t{}MCs\tGrade: {}".format(
+            index, mod['name'], mod['mc'], mod['gradepoint']
+        ))
+    inp = input(INDICES_PROMPT)
+    while inp.lower() != 'stop':
+        all_mods[int(index)] = None
+        inp = input(INDICES_PROMPT)
+    return list(filter(lambda x: x is not None, all_mods))
+>>>>>>> 42b2096786153535428ed8581e6d4ea8ab80e8b1
 
 all_mods = input_all_mods()
 print("your cap is", calc_cap(all_mods))
-if input("do you want to s/u? y/n: ") == 'y':
+if input(SU_PROMPT) == 'y':
     all_mods = su(all_mods)
     print("your new cap is", calc_cap(all_mods))
